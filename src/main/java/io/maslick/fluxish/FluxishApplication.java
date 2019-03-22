ba2311fa991e0e0b9b2f389d0990f9eed73803f4
Pavel Maslov <pavel.maslov@ineor.si>
2019-03-22 16:47:23 +0100

    init commit

---
+++ src/main/java/io/maslick/fluxish/FluxishApplication.java
@@ -0,0 +1,11 @@
+package io.maslick.fluxish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxishApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FluxishApplication.class, args);
+	}
+}
