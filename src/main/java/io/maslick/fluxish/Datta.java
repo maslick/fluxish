1ce928b53ef8192ea3f4831dd11cfec78032772f
Pavel Maslov <pavel.maslov@ineor.si>
2019-03-23 12:36:53 +0100

    using jaxb instead of jackson-dataformat-xml

--- src/main/java/io/maslick/fluxish/Datta.java
+++ src/main/java/io/maslick/fluxish/Datta.java
@@ -3,10 +3,12 @@ package io.maslick.fluxish;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlRootElement
 public class Datta {
 	private String title;
 }
